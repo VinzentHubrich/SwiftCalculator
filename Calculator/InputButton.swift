abc90d91ca41b441d171bce0fb9c6f701d2ca7b9
Vinzent Hubrich <hubrichv@gmail.com>
2023-12-12 14:52:02 +0100

    Add input buttons

---
+++ Calculator/InputButton.swift
@@ -0,0 +1,18 @@
+//
+//  InputButton.swift
+//  Calculator
+//
+//  Created by Vinzent Hubrich on 12.12.23.
+//
+
+import SwiftUI
+
+struct InputButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputButton()
+}
