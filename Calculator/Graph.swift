ef6e1d6ffca142d7933ad5fcb640b93f906dc4a1
Vinzent Hubrich <hubrichv@gmail.com>
2024-01-06 21:01:19 +0100

    Refactor graph

--- Calculator/Graph.swift
+++ Calculator/Graph.swift
@@ -8,60 +8,76 @@
 import SwiftUI
 import Charts
 
+struct Point: Identifiable {
+    var id = UUID().uuidString
+    var x: Double
+    var y: Double
+}
+
 struct Graph: View {
-    let expression: String = "x^2"
+    let expression: String
+    
+    @State var points: [Point] = []
     
     var domainX: [Double] = [-10, 10]
     var domainY: [Double] = [-10, 10]
-    let resolution: Int = 100
+    let resolution: Int = 15
+    let frequency: Double
 
-    private func frequency() -> Double {
-        (domainX.last! - domainX.first!) / Double(resolution)
+    init(expression: String) {
+        self.expression = expression
+        self.frequency = (domainX.last! - domainX.first!) / Double(resolution)
     }
     
-    private func xValues() -> [Double] {
-        Array(stride(from: domainX.first!, through: domainX.last!, by: frequency()))
-    }
-    
-    private func calculateY(_ x: Double) -> Double {
+    private func calculatePoints() {
         var expr: [String] = expression.map { String($0) }
         
-        // Replace x and insert * operation if necessary
+        // Insert * operation before x if necessary
         while let index = expr.firstIndex(where: { $0 == "x" }) {
             if index > 0 && Double(expr[index-1]) != nil {
-                expr[index] = "*\(x)"
+                expr[index] = "*X"
             } else {
-                expr[index] = String(x)
+                expr[index] = "X"
             }
         }
         
-        let result = evaluateMathExpression(expr.joined())
+        // Calculate values
+        var values: [Point] = []
         
-        if result == nil {
-            return Double.nan
+        for x in Array(stride(from: domainX.first!, through: domainX.last!, by: frequency)) {
+            let result = evaluateMathExpression(String(expr.joined()).replacingOccurrences(of: "X", with: String(x)))
+            
+            if result == nil {
+                values.append(Point(x: x, y: Double.nan))
+            } else {
+                values.append(Point(x: x, y: Double(result!)!))
+            }
         }
         
-        return Double(result!)!
+        points = values
     }
     
     var body: some View {
         Chart {
-            RuleMark(x: .value("", 0)).foregroundStyle(.black)
-            RuleMark(y: .value("", 0)).foregroundStyle(.black)
+            RuleMark(x: .value("", 0))
+            RuleMark(y: .value("", 0))
             
-            ForEach(xValues(), id: \.self) { x in
+            ForEach(points) { point in
                 LineMark(
-                    x: .value("", x),
-                    y: .value("", calculateY(x))
+                    x: .value("", point.x),
+                    y: .value("", point.y)
                 )
-                //.interpolationMethod(.monotone)
+                .foregroundStyle(.orange)
+                .interpolationMethod(.monotone)
             }
         }
+        .foregroundStyle(.white)
         .chartXScale(domain: domainX)
         .chartYScale(domain: domainY)
+        .onAppear { calculatePoints() }
     }
 }
 
 #Preview {
-    Graph()
+    Graph(expression: "1/x").background(.black)
 }
